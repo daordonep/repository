970a6d3e1aae2b0db6c0a84fad98ad0bfb1c80c3
Dharian <dopi.xd2@gmail.com>
2021-12-30 17:26:58 +0100

    Initial commit

---
+++ src/test/java/com/daordonep/wishlist/RepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daordonep.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
